a687a147c63f083f03cbf6127fbb955396b0fbf0
jason <jason@hedmans.org>
2022-11-12 14:33:57 -0600

    blocks in action

--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 
-import useVault from '../../hooks/useVault';
-
 import { 
-  Box, 
   Flex
 } from '@chakra-ui/react';
 
@@ -11,11 +8,9 @@ import ProductInfo from './ProductInfo';
 import Actions from '../Vault/Actions';
 
 import { vaultManager } from '../../data/vaultManager';
-import PriceChart from '../Vault/PriceChart';
-import Strategies from '../Strategies';
-import Card from '../utilities/Card';
 import useStructuredProduct from '../../hooks/useStructuredProduct';
 import { getStructuredProduct } from '../../data/structuredProducts';
+import { structToString } from '../../services/vaults';
 
 interface Props {
   productName: string;
@@ -44,17 +39,13 @@ const ProductComponent : React.FC<Props> = ({ productName }) => {
         <ProductInfo
           name={structuredProduct.name}
           blocks={structuredProduct.blocks}
-          // vault={vault}
         />
-        <Actions 
+        <Actions
           deposit={deposit}
           withdraw={withdraw}
-          coinAddress={structuredProduct.coinStruct}
-          coinSymbol={structuredProduct.coinSymbol}
-          coinLogo={`/${structuredProduct.coinSlug}_logo.jpeg`}
+          block={structuredProduct.block}
         />
       </Flex>
-      {/* <PriceChart /> */}
     </Flex>
   )
 }
