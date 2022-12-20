0652511a6b02e5e0af2a41682d133f3f3732732b
jason <jason@hedmans.org>
2022-12-20 16:02:45 -0500

    remove CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+
 import Home from '../components/Home'
+
 import DefaultLayout from '../layouts/default'
-import styles from '../styles/Home.module.css'
 
 const HomePage: NextPage = () => {
   return (
