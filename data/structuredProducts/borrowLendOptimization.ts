f9c16b1b83fbc057134cb8a80786d0656a112181
jason <jason@hedmans.org>
2023-01-11 16:38:31 -0500

    fix wording

--- data/structuredProducts/borrowLendOptimization.ts
+++ data/structuredProducts/borrowLendOptimization.ts
@@ -5,7 +5,7 @@ import { StructuredProduct } from '../../types/structuredProduct'
 const borrowLendOptimization: StructuredProduct = {
     moduleAddress: "0x111",
     name: "Borrow/Lend Optimization",
-    description: "Lend USDC to the borrow/lend market offering the highest APY. Periodically calculate and deploy assets to the highest-earning pools.",
+    description: "Lend USDC to the borrow/lend market offering the highest APY. Periodically identify and deploy assets to the highest-earning pools.",
     protocols: ["aries", 'aptin', 'abel'],
     blocks: [lendUSDCToAries, lendUSDCToAptin, lendUSDCToAbel],
     block: lendUSDCThroughSatay,
